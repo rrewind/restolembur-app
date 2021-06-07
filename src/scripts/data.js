3fa5ac6ec63e57f245d4c32c2cde4f4bf0cadc9a
lapan13 <ariirwandi13@gmail.com>
2021-06-07 17:01:56 +0700

    add link for home menu

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,6 +4,4 @@ let resto = list.restaurants;
 $.each(resto, function(idx, val) {
 let card = '<div class="list-item"><div class="img-container"><img class="img-res list-item_thumbnail" src="'+val.pictureId+'" alt="'+val.name+'"><em title="ratings" class="list-item_rating fa fa-star "><span> '+val.rating+'</span></em><span class="list-item_title">'+val.name+' - <a href="#" class="list-item_city">'+val.city+'</a></span></div><div class="list-item_content"><p class="truncate list-item_description">'+val.description+'</p></div></div>'
     $(".lists").append(card);
-    console.log(val);
-    console.log(val.pictureId)
 });
